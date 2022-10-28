5428a38ee4d020b54de53e6e5544b6178f7e0080
tmoh7 <tmoh7383@gmail.com>
2022-10-28 12:22:38 -0600

    fixed #1

--- word-repeater.py
+++ word-repeater.py
@@ -4,10 +4,9 @@ def main():
     """
     word = input("Enter word to repeat: ")
     n = int(input("Enter number of times to repeat: "))
-
-    for i in range(1, n):
+    for i in range(0, n):
         print(word)
 
-
+#fix
 if __name__ == "__main__":
     main()
